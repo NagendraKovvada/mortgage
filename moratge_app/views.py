6f8b3c752edfe4e1dec33b19b7daf9a9268b319e
NagendraKovvada <nagendra.kovvada@accionlabs.com>
2019-03-31 03:54:44 +0530

    Code Format

--- moratge_app/views.py
+++ moratge_app/views.py
@@ -5,17 +5,6 @@ from .models import *
 from weasyprint import HTML,CSS
 from django.template.loader import render_to_string
 from django.core.files.storage import FileSystemStorage
-
-
-
-# class html_to_pdf_view(TemplateView):
-# 	template_name = 'client.html'
-
-# 	def get_context_data(self,**kwargs):
-# 		query = Client.objects.all()		
-# 		context={'form':query}
-# 		return context
-
 from django.conf import settings
 
 def html_to_pdf_view(request,id=None):
@@ -24,10 +13,7 @@ def html_to_pdf_view(request,id=None):
 	html_string = render_to_string('loan.html', {'form': query})
 	# stylesheets=[CSS(settings.STATIC_ROOT +  'css/main.css')]CSS(settings.STATIC_ROOT +  'css/main.css')]
 	html = HTML(string=html_string, base_url=request.build_absolute_uri())
-	# stylesheets=[CSS('style.css')]
-	# html.write_pdf(target='/tmp/mypdf.pdf');
 	html.write_pdf(target='mortgage.pdf',stylesheets=[CSS(settings.STATIC_ROOT +  '/style.css')]);
-		# HTML('client.html').write_pdf('/tmp/mypdf.pdf')
 	fs = FileSystemStorage()
 	with fs.open('mortgage.pdf') as pdf:
 		response = HttpResponse(pdf, content_type='application/pdf')
